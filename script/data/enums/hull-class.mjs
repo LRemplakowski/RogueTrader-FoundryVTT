7f66663b41b484d8d1eac2072e7937fa2c517054
Switchu <remplak@gmail.com>
2026-05-28 00:40:38 +0200

    fixed runtime errors

--- script/data/enums/hull-class.mjs
+++ script/data/enums/hull-class.mjs
@@ -1,16 +1,16 @@
 import { EnumBase } from "./enum-base.mjs";
 
 export default class HullClass extends EnumBase {
-  static DEFAULT = "Transport";
+  static DEFAULT = "transport";
 
   static DATA = {
-    Transport: { label: "HULL_CLASS.TRANSPORT" },
-    Raider: { label: "HULL_CLASS.RAIDER" },
-    Frigate: { label: "HULL_CLASS.FRIGATE" },
-    "Light Cruiser": { label: "HULL_CLASS.LIGHT_CRUISER" },
-    Cruiser: { label: "HULL_CLASS.CRUISER" },
-    Battlecruiser: { label: "HULL_CLASS.BATTLECRUISER" },
-    "Grand Cruiser": { label: "HULL_CLASS.GRAND_CRUISER" },
-    Battleship: { label: "HULL_CLASS.BATTLESHIP" }
+    transport: { label: "HULL_CLASS.TRANSPORT" },
+    raider: { label: "HULL_CLASS.RAIDER" },
+    frigate: { label: "HULL_CLASS.FRIGATE" },
+    lightCruiser: { label: "HULL_CLASS.LIGHT_CRUISER" },
+    cruiser: { label: "HULL_CLASS.CRUISER" },
+    battlecruiser: { label: "HULL_CLASS.BATTLECRUISER" },
+    grandCruiser: { label: "HULL_CLASS.GRAND_CRUISER" },
+    battleship: { label: "HULL_CLASS.BATTLESHIP" }
   };
 }
